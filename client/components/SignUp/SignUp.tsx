61155a79717d0407105a7a578da725650e71bcad
andrewmcgov <andrew.mcgoveran@shopify.com>
2019-06-23 18:29:04 -0400

    Add signup key to account registration

--- client/components/SignUp/SignUp.tsx
+++ client/components/SignUp/SignUp.tsx
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import { Mutation, MutationFn } from 'react-apollo';
 
 import { CURRENT_USER_QUERY } from '../queries';
+import Error from '../Error';
 
 const SIGN_UP_MUTATION = gql`
   mutation SIGN_UP_MUTATION(
@@ -11,6 +12,7 @@ const SIGN_UP_MUTATION = gql`
     $email: String
     $password: String
     $repeatPassword: String
+    $signupKey: String
   ) {
     createUser(
       firstName: $firstName
@@ -18,6 +20,7 @@ const SIGN_UP_MUTATION = gql`
       email: $email
       password: $password
       repeatPassword: $repeatPassword
+      signupKey: $signupKey
     ) {
       firstName
       lastName
@@ -32,6 +35,7 @@ function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
+  const [signupKey, setSignupKey] = useState('');
 
   function handleFormSubmit(
     e: React.FormEvent<HTMLFormElement>,
@@ -44,7 +48,8 @@ function SignIn() {
         lastName,
         email,
         password,
-        repeatPassword
+        repeatPassword,
+        signupKey
       }
     });
   }
@@ -54,82 +59,96 @@ function SignIn() {
       mutation={SIGN_UP_MUTATION}
       refetchQueries={[{ query: CURRENT_USER_QUERY }]}
     >
-      {(signUp: MutationFn, { loading }) => (
-        <div className="form-card account-form-card">
-          <h3 className="account-form-heading">Sign up!</h3>
-          <form
-            method="post"
-            onSubmit={e => handleFormSubmit(e, signUp)}
-            className="signin-form"
-          >
-            <fieldset disabled={loading}>
-              <div className="form-input-pairing">
-                <label htmlFor="signin:firstName">
-                  First name
+      {(signUp: MutationFn, { loading, error }) => (
+        <>
+          {error && <Error error={error} />}
+          <div className="form-card account-form-card">
+            <h3 className="account-form-heading">Sign up!</h3>
+            <form
+              method="post"
+              onSubmit={e => handleFormSubmit(e, signUp)}
+              className="signin-form"
+            >
+              <fieldset disabled={loading}>
+                <div className="form-input-pairing">
+                  <label htmlFor="signin:firstName">
+                    First name
+                    <input
+                      type="text"
+                      name="firstName"
+                      placeholder="First name"
+                      id="signup:firstName"
+                      value={firstName}
+                      onChange={e => setFirstName(e.target.value)}
+                    />
+                  </label>
+                  <label htmlFor="signin:lastName">
+                    Last Name
+                    <input
+                      type="text"
+                      name="lastName"
+                      placeholder="Last name"
+                      id="signup:lastName"
+                      value={lastName}
+                      onChange={e => setLastName(e.target.value)}
+                    />
+                  </label>
+                </div>
+                <label htmlFor="signin:email">
+                  Email
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    id="signup:email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                  />
+                </label>
+                <div className="form-input-pairing">
+                  <label htmlFor="signin:password">
+                    Password
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="password"
+                      id="signup:password"
+                      value={password}
+                      onChange={e => setPassword(e.target.value)}
+                    />
+                  </label>
+                  <label htmlFor="signin:repeatPassword">
+                    Repeat Password
+                    <input
+                      type="password"
+                      name="repeatPassword"
+                      placeholder="Repeat password"
+                      id="signup:repeatPassword"
+                      value={repeatPassword}
+                      onChange={e => setRepeatPassword(e.target.value)}
+                    />
+                  </label>
+                </div>
+                <label htmlFor="signin:signupKey">
+                  Singup Key
                   <input
                     type="text"
-                    name="firstName"
-                    placeholder="First name"
-                    id="signup:firstName"
-                    value={firstName}
-                    onChange={e => setFirstName(e.target.value)}
+                    name="signupKey"
+                    placeholder="Singup key"
+                    id="signup:signupKey"
+                    value={signupKey}
+                    onChange={e => setSignupKey(e.target.value)}
                   />
                 </label>
-                <label htmlFor="signin:lastName">
-                  Last Name
-                  <input
-                    type="text"
-                    name="lastName"
-                    placeholder="Last name"
-                    id="signup:lastName"
-                    value={lastName}
-                    onChange={e => setLastName(e.target.value)}
-                  />
-                </label>
-              </div>
-              <label htmlFor="signin:email">
-                Email
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="email"
-                  id="signup:email"
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
-                />
-              </label>
-              <div className="form-input-pairing">
-                <label htmlFor="signin:password">
-                  Password
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="password"
-                    id="signup:password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                  />
-                </label>
-                <label htmlFor="signin:repeatPassword">
-                  Repeat Password
-                  <input
-                    type="password"
-                    name="repeatPassword"
-                    placeholder="Repeat password"
-                    id="signup:repeatPassword"
-                    value={repeatPassword}
-                    onChange={e => setRepeatPassword(e.target.value)}
-                  />
-                </label>
-              </div>
-              <div className="account-form-buttons">
-                <button className="button button-primary" type="submit">
-                  Sign up!
-                </button>
-              </div>
-            </fieldset>
-          </form>
-        </div>
+                <div className="account-form-buttons">
+                  <button className="button button-primary" type="submit">
+                    Sign up!
+                  </button>
+                </div>
+              </fieldset>
+            </form>
+          </div>
+        </>
       )}
     </Mutation>
   );
